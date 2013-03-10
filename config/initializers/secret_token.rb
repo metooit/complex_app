eef7984d0c5ba86b72b7f5295bb06ee77f8f714f
metooit <oliver.lee.me@gmail.com>
2013-03-10 15:58:52 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComplexApp::Application.config.secret_token = '4e7edd755aebdd100ea8db2f96f93dd33d7fedefa1571a35a6efcf4b2bee1f306d8f16e43cd52d0cf4c58700822e1a5a261bdf06c2f5ad74b76579da12098a78'
